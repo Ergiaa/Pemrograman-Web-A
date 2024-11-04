ada37398cec5e2d96ca0575515d33a973564aac5
Viera Tito <129572101+Ergiaa@users.noreply.github.com>
2024-11-04 15:39:42 +0700

    Pertemuan 11

--- app/5026221096/app/Http/Controllers/DosenController.php
+++ app/5026221096/app/Http/Controllers/DosenController.php
@@ -13,4 +13,16 @@ class DosenController extends Controller
     public function blog(){
         return view('blog');
     }
+
+    public function biodata(){
+        $data = [
+            'nama' => 'Viera Tito Virgiawan',
+            'umur' => 20,
+            'hasil' => 20 * 2
+        ];
+
+    	$pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+
+    	return view('biodata',['jsonData' => $data , 'matkul' => $pelajaran]);
+    }
 }
